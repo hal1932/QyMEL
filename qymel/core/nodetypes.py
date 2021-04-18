1a6584ded2f46c6e7924251b1bdb3cc3fb7ea35d
hal1932 <yu.arai.19@gmail.com>
2021-04-18 18:56:59 +0900

    リファレンスの作成はSceneクラスに移譲

--- qymel/core/nodetypes.py
+++ qymel/core/nodetypes.py
@@ -1353,22 +1353,6 @@ class FileReference(DependNode):
     _mfn_set = _om2.MFnReference
     _mel_type = 'reference'
 
-    @staticmethod
-    def create(file_path, namespace=None, depth=None):
-        # type: (str, str, str) -> FilReference
-        kwargs = {
-            'reference': True,
-            'namespace': os.path.splitext(os.path.basename(file_path))[0]
-        }
-        if namespace is not None:
-            kwargs['namespace'] = namespace
-        if depth is not None:
-            kwargs['loadReferenceDepth'] = depth
-
-        file = _cmds.file(file_path, **kwargs)
-        node = _cmds.referenceQuery(file, referenceNode=True)
-        return FileReference(node)
-
     @property
     def file_path(self):
         # type: () -> str
