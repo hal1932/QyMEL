89a609b9df479a659fd9af4643db901726fac56e
hal1932 <yu.arai.19@gmail.com>
2021-04-26 01:19:25 +0900

    サンプルコードを追加

--- qymel/ui/widgets/expander.py
+++ qymel/ui/widgets/expander.py
@@ -127,3 +127,20 @@ class Expander(QFrame):
             self.expand()
         else:
             self.collapse()
+
+
+if __name__ == '__main__':
+    widget = QWidget()
+    layout = QVBoxLayout()
+    widget.setLayout(layout)
+
+    for _ in range(3):
+        expander = Expander()
+        expander.header_layout.setContentsMargins(1, 1, 1, 1)
+        expander.content_layout.setContentsMargins(1, 1, 1, 1)
+        expander.set_header_widget(QLabel('aaa'))
+        expander.set_content_widget(QPushButton('bbb'))
+        layout.addWidget(expander)
+    layout.addStretch()
+
+    widget.show()
