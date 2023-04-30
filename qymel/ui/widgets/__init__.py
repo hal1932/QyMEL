cd7a4f946900b002162dcb78ae2bbb33c86905d4
hal1932 <yu.arai.19@gmail.com>
2023-05-01 04:13:39 +0900

    機能追加

--- qymel/ui/widgets/__init__.py
+++ qymel/ui/widgets/__init__.py
@@ -7,36 +7,72 @@ from six.moves import *
 from ..pyside_module import *
 
 
-def hline():
-    # type: () -> QFrame
-    frame = QFrame()
+def hline(parent=None):
+    # type: (QObject) -> QFrame
+    frame = QFrame(parent)
     frame.setFrameShape(QFrame.HLine)
     frame.setFrameShadow(QFrame.Sunken)
     return frame
 
 
-def vline():
-    # type: () -> QFrame
-    frame = QFrame()
+def vline(parent=None):
+    # type: (QObject) -> QFrame
+    frame = QFrame(parent)
     frame.setFrameShape(QFrame.VLine)
     frame.setFrameShadow(QFrame.Sunken)
     return frame
 
 
-def hseparator():
-    # type: () -> QFrame
-    frame = QFrame()
+def hseparator(parent=None):
+    # type: (QObject) -> QFrame
+    frame = QFrame(parent)
     frame.setFrameShape(QFrame.HLine)
     frame.setFrameShadow(QFrame.Sunken)
     return frame
 
 
-def vseparator():
-    # type: () -> QFrame
-    frame = QFrame()
+def vseparator(parent=None):
+    # type: (QObject) -> QFrame
+    frame = QFrame(parent)
     frame.setFrameShape(QFrame.VLine)
     frame.setFrameShadow(QFrame.Sunken)
     return frame
+
+
+def groupbox(title, layout, parent=None):
+    # type: (text_type, QLayout, QObject) -> QGroupBox
+    box = QGroupBox(title, parent)
+    box.setLayout(layout)
+    return box
+
+
+def scroll_area(layout, parent=None):
+    # type: (QLayout, QObject) -> QScrollArea
+    area = QScrollArea(parent)
+    widget = QWidget(area)
+    widget.setLayout(layout)
+    area.setWidget(widget)
+    area.setWidgetResizable(True)
+    return area
+
+
+def image(pixmap_or_filepath, parent=None):
+    # type: (Union[QPixmap, text_type], QObject) -> QLabel
+    label = QLabel(parent)
+    pix = pixmap_or_filepath if isinstance(pixmap_or_filepath, QPixmap) else QPixmap(pixmap_or_filepath)
+    label.setPixmap(pix)
+    return label
+
+
+def image_button(pixmap_or_filepath, parent=None):
+    # type: (Union[QPixmap, text_type], QObject) -> QPushButton
+    button = QPushButton(parent)
+    pix = pixmap_or_filepath if isinstance(pixmap_or_filepath, QPixmap) else QPixmap(pixmap_or_filepath)
+    icon = QIcon(pix)
+    button.setIcon(icon)
+    button.setIconSize(pix.size())
+    button.setFixedSize(pix.size())
+    return button
 
 
 def desktop():
